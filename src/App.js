2771e0125a8d712491705cb029ae77b9b3355978
Razan Harashe <harashe115@gmail.com>
2024-06-27 14:26:11 +0300

    add error messages.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,56 @@ function App() {
     confirmPassword: ''
   });
 
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value
     });
+
+    // Validation logic
+    switch (name) {
+      case 'firstName':
+        setErrors({
+          ...errors,
+          firstName: value.length < 2 ? 'First Name must be at least 2 characters' : ''
+        });
+        break;
+      case 'lastName':
+        setErrors({
+          ...errors,
+          lastName: value.length < 2 ? 'Last Name must be at least 2 characters' : ''
+        });
+        break;
+      case 'email':
+        setErrors({
+          ...errors,
+          email: value.length < 5 ? 'Email must be at least 5 characters' : ''
+        });
+        break;
+      case 'password':
+        setErrors({
+          ...errors,
+          password: value.length < 8 ? 'Password must be at least 8 characters' : ''
+        });
+        break;
+      case 'confirmPassword':
+        setErrors({
+          ...errors,
+          confirmPassword: value !== formData.password ? 'Passwords must match' : ''
+        });
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -29,6 +73,7 @@ function App() {
             value={formData.firstName}
             onChange={handleChange}
           />
+          {formData.firstName && <span className="error">{errors.firstName}</span>}
         </div>
         <div>
           <label>Last Name</label>
@@ -38,6 +83,7 @@ function App() {
             value={formData.lastName}
             onChange={handleChange}
           />
+          {formData.lastName && <span className="error">{errors.lastName}</span>}
         </div>
         <div>
           <label>Email</label>
@@ -47,6 +93,7 @@ function App() {
             value={formData.email}
             onChange={handleChange}
           />
+          {formData.email && <span className="error">{errors.email}</span>}
         </div>
         <div>
           <label>Password</label>
@@ -56,6 +103,7 @@ function App() {
             value={formData.password}
             onChange={handleChange}
           />
+          {formData.password && <span className="error">{errors.password}</span>}
         </div>
         <div>
           <label>Confirm Password</label>
@@ -65,6 +113,7 @@ function App() {
             value={formData.confirmPassword}
             onChange={handleChange}
           />
+          {formData.confirmPassword && <span className="error">{errors.confirmPassword}</span>}
         </div>
       </form>
       <div>
